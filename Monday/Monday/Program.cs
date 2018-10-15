75feda2effb9c5a41366a1d56703431f05d05774
Amber Cook <30010451@student.toiohomai.ac.nz>
2018-10-15 16:29:57 +1300

    not done

---
+++ Monday/Monday/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monday
+{ //Monday 15/10/18
+    class Program
+    {
+        //Revision TASK 1:
+        static void Main(string[] args)
+        {
+            string[] names = new string[5];
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"Please enter the name {i}");
+                names[i] = Console.ReadLine();
+            }
+
+
+        }
+
+        //Revision TASK 2:
+        public static void task2()
+        {
+            int[] nums = new int[10];
+            Random rand = new Random();
+            int total = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rand.Next(1, 10);
+                total = +nums[i];
+            }
+
+            Console.WriteLine($"Average of the ten number in the array is {total / nums.Length}");
+        }
+
+        //Arrays Extended Practise Exercises - Task 1:
+        public static void extended()
+        {
+            int[] numbers = new int[50];
+            Random rand = new Random();
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                numbers[j] = rand.Next(1, 100);
+
+            }
+            Array.Sort(numbers);
+        }
+    }
+}
+
